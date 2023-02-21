da6c388310a5fa261fbb59468b34290887e231ca
amaye15 <andrewmayes14@hotmail.com>
2023-02-21 04:12:10 +0100

    delete fixed v2

--- app.py
+++ app.py
@@ -131,7 +131,7 @@ def main():
             sl.text("Note : le changement n'apparaîtra pas tant que vous n'aurez pas ajouté une nouvelle ligne.")
         # Delete button
         if delete_button: 
-            match_worksheet.delete_row(row_choice)
+            match_worksheet.delete_row(row_choice + 1)
             sl.experimental_rerun()
 
         ### After Match ###
