307e5ccd3b7eee35a086907207b511245f07a4ca
amaye15 <andrewmayes14@hotmail.com>
2023-02-21 04:30:34 +0100

    cred test v2

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 def main():
     # Connect to Google
     #gc = gspread.service_account(filename="cred.json")
-    gc = gspread.service_account_from_dict(sl.secrets["google_drive_api"])
+    print(sl.secrets["google_drive_api"])
+    gc = gspread.service_account_from_dict(dict(sl.secrets["google_drive_api"]))
 
     ### Image ###
     _, center, _ = sl.columns([1, 1, 1])
